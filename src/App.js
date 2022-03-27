8de60f27fe32b2b2709efc1024d555f096749523
Derek <nightwing917@hotmail.com>
2022-03-26 19:29:14 -0800

    working on app js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends Component {
+  render() {
+    return <div className="App" />
+  }
 }
 
 export default App;
