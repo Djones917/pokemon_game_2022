253ec1c7c3ef4a7081f9be683570571c97a8dbab
Derek <nightwing917@hotmail.com>
2022-03-30 19:36:18 -0800

    adding charmander

--- src/App.js
+++ src/App.js
@@ -6,11 +6,7 @@ class App extends Component {
   render() {
     return <div className="App">
       <Pokecard
-        id={4}
-        name="Charmander"
-        type="fire"
-        exp={62} 
-      />
+        id={4} name="Charmander" type="fire" exp={62} />
     </div>
   };
 }
