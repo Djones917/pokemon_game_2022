472d7bd3596c8bf089cb5a9feefe536b06a8c15a
Derek <nightwing917@hotmail.com>
2022-03-30 19:40:16 -0800

    added h1 for charmander

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -4,7 +4,7 @@ class Pokecard extends Component {
     render() {
         return (
             <div className="Pokecard">
-
+              <h1>{this.props.name}</h1>
             </div>
         )
     }
