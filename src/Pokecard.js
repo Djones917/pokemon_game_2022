86e3fd3a64c41ef09da7bab7baf9558d82a49659
Derek <nightwing917@hotmail.com>
2022-04-01 19:30:17 -0800

    tyring to fix typo for image

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -3,9 +3,11 @@ const POKE_API = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprit
 
 class Pokecard extends Component {
     render() {
+        let imgSrc = '${POKE_API}${this.props.id}.png';
         return (
             <div className="Pokecard">
               <h1>{this.props.name}</h1>
+              <img src={imgSrc} />
             </div>
         )
     }
