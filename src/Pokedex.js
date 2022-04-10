74f083d3ae6db7e89011b43e0439d1864ba71314
Derek <nightwing917@hotmail.com>
2022-04-09 19:31:58 -0800

    importing pokecard to pokedex

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Pokecard from './Pokecard';
 
 class Pokedex extends Component{
     static defaultProps = {
@@ -16,7 +17,7 @@ class Pokedex extends Component{
    render() {
        return (
            <div className='Pokedex'>
-
+              <h1>Pokedex!</h1>
            </div>
        )
    }
