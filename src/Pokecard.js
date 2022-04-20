1c2825340d5b6d6816f0b26dde57c096fa06983c
Derek <nightwing917@hotmail.com>
2022-04-19 20:21:22 -0800

    added terinary operator

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -4,6 +4,8 @@ import './Pokecard.css';
 //const POKE_API = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
 const POKE_API = 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/';
 
+let padToThree = (number) => (number <= 999 ? `00${number}`.slice(-3): number);
+
 class Pokecard extends Component {
     render() {
         let imgSrc = `${POKE_API}${this.props.id}.png`;
