e4819097f040b66fe9ce9cae60ca4da15ee352e4
Derek <nightwing917@hotmail.com>
2022-04-24 20:31:52 -0800

    added rand pokemon variable

--- src/Pokegame.js
+++ src/Pokegame.js
@@ -17,7 +17,8 @@ class Pokegame extends Component {
         let hand1 = [];
         let hand2 = [...this.props.pokemon];
         while(hand1.length < hand2.length0) {
-            let randIdx = Math.floor(Math.random() * hand2.length)
+            let randIdx = Math.floor(Math.random() * hand2.length);
+            let randPokemon = hand2.splice(randIdx, 1); 
         }
         return (
             <div>
