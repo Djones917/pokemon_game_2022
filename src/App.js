fafacdc24e07f594929884964f517411c678cfc2
Derek <nightwing917@hotmail.com>
2022-05-04 19:45:56 -0800

    hopefully managed files

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
-import Pokedex from './Pokedex';
+import Pokegame from './Pokegame';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return <div className="App">
-      <Pokedex />
-    </div>
-  };
+	render() {
+		return (
+			<div className="App">
+				<Pokegame />
+			</div>
+		);
+	}
 }
 
 export default App;
